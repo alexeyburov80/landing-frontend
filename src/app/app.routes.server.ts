85859614771238f192461fbdc93dd8d0b56289f1
alexeyburov <alexeyburov80@gmail.com>
2025-08-11 16:41:22 +0400

    fix scroll style

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,3 +1,26 @@
-import { ServerRoute } from '@angular/ssr';
+import {RenderMode, ServerRoute} from '@angular/ssr';
 
-export const serverRoutes: ServerRoute[] = [];
+
+export const serverRoutes: ServerRoute[] = [
+  {
+    path: '',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'contact',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'products',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'response',
+    renderMode: RenderMode.Server
+  },
+  // Wildcard route для SSR
+  {
+    path: '**',
+    renderMode: RenderMode.Server
+  }
+];
